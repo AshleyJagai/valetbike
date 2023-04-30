ff50e5615fe760c17a6c9001114c2b19782fbf86
Priya Dalal-Whelan <priyadw00@gmail.com>
2023-04-30 13:12:28 -0400

    added age validation to user, made email unique. still not displaying validation error msgs

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,34 @@ class User < ApplicationRecord
     validates_length_of :username, minimum: 1 
 
     validates_presence_of :email
+    validates_uniqueness_of :email 
 
     validates_presence_of :phone_number
 
     validates_presence_of :credit
     validates_numericality_of :credit, greater_than_or_equal_to: 0
 
-    #validates_
+    validates_presence_of :date_birth
+    validate :user_age 
 
-    # username 
-    #password 
-    #email 
-    #addy  
-    # phone number  #only numbers 
-    #credits # not being a number, less then zero, more then 2 decimal places 
+    #creds to this stack overflow post https://stackoverflow.com/questions/40457247/id-like-to-validate-a-birthdate-attribute-to-be-over-18-years-old 
 
+    def user_age 
+       if date_birth.present? && date_birth > 18.years.ago 
+            errors.add( :date_birth, 'you must be at least 18 years old to use valetscoot')
+    end 
+end
+
+
+#    validates "user age"
+
+#     private 
+
+#         def user_age 
+#             if age. 
+#                 errors.add(:attribute, "you must be at least 18 to use valetscoot")
+#             end 
+#         end 
     
     
 
